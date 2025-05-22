47c4dfc61befbdd1722a24ab35eb5e342f209715
michisthr <michlstoehr19@icloud.com>
2025-05-22 11:28:42 +0200

    add Bits class

--- register.py
+++ register.py
@@ -1,37 +1,29 @@
 from loguru import logger
-
+from bits import Bits
 
 class Registers:
     def __init__(self, size: int):
         self.size = size
-        self.registers: list = ["0" * self.size] * self.size
+        self.registers: list[Bits] = [Bits(self.size)] * self.size
         self.register_präfix = "x"
 
     def print_registers(self):
         for idx, register in enumerate(self.registers):
             print(f"x{idx} \t{register}")
 
-    def get_register(self, idx: int) -> str:
+    def get_register(self, idx: int) -> Bits:
         try:
-            return register.get(idx)
+            return self.registers[idx]
         except Exception as ex:
             logger.error(f"Failed to get register: {idx}. Exception: {ex}")
 
-    def set_register(self, idx: int, value: str):
+    def set_register(self, idx: int, bits: Bits):
         try:
-            if len(value) != self.size:
-                raise Exception(
-                    f"The value length should be 32. The current value is: {value} with lenght: {len(value)}"
-                )
-            if idx >= self.size - 1 or idx <= 0:
-                raise Exception(
-                    f"The index should be between {0} and {self.size - 1}. It is: {idx}"
-                )
-            self.registers[idx] = value
-            logger.info(f"Register[{idx}] set to {value}")
+            self.registers[idx] = bits
+            logger.info(f"Register[{idx}] set to {bits}")
         except Exception as ex:
             logger.error(
-                f"Failed to set register: {idx} with value: {value}. Exception: {ex}"
+                f"Failed to set register: {idx} with value: {bits}. Exception: {ex}"
             )
 
     def get_registers(self) -> list:
@@ -40,11 +32,9 @@ class Registers:
 
 if __name__ == "__main__":
     register = Registers(size=32)
-    register.set_register(2, "000000000000000")
-    register.set_register(49, "0" * 32)
-    register.set_register(-1, "0" * 32)
-    register.set_register(4, "0" * 32)
-    register.set_register(8, "01" * 16)
-    register.set_register(20, "1" * 32)
-    register.set_register(20, "" * 32)
+    bits = Bits(32)
+    register.set_register(2, bits)
+    register.print_registers()
+    print("\n\n")
+    bits.set_at_index(4, "1")
     register.print_registers()
