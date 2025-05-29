1e517e4ba518a86bfd791c783680b061badedc3f
michisthr <michlstoehr19@icloud.com>
2025-05-29 21:47:53 +0200

    implement _li function

--- src/model/cpu.py
+++ src/model/cpu.py
@@ -17,27 +17,34 @@ class CPU:
             # run specific function
             getattr(self, f"_{function}")(*args)
         except AttributeError:
-            logger.warning("Instruction is not defined")
+            logger.error("Instruction is not defined")
         except TypeError as e:
-            logger.warning(f"Invalid arguments for instruction: {e}")
+            logger.error(f"Invalid arguments for instruction: {e}")
+        except Exception as ex:
+            logger.error(ex)
+
+    def get_register_index(self, r: str):
+        try:
+            return int(r.removeprefix("x"))
+        except ValueError:
+            raise ValueError(
+                f"You can't run instruction with following values: {r}. This is the preferred scheme: 'x1'"
+            )
+
+    def get_imm(self, imm: str):
+        try:
+            return int(imm)
+        except ValueError:
+            raise ValueError(f"You can't get imm with following value: {imm}")
 
     def _mov(self, x1, x2, x3):
         logger.info(f"Run mov with args: {x1}, {x2}, {x3}")
 
     def _add(self, rd: str, rs1: str, rs2: str):
         logger.info(f"Run add with rd={rd}, rs1={rs1}, rs2={rs2}")
-        rd = rd.removeprefix("x")
-        rs1 = rs1.removeprefix("x")
-        rs2 = rs2.removeprefix("x")
-
-        try:
-            rd = int(rd)
-            rs1 = int(rs1)
-            rs2 = int(rs2)
-        except ValueError:
-            logger.error(
-                f"You can't run instructions with following values: {rd}, {rs1}, {rs2}. This is the preferred scheme: 'x1'"
-            )
+        rd = self.get_register_index(rd)
+        rs1 = self.get_register_index(rs1)
+        rs2 = self.get_register_index(rs2)
 
         self.registers[rd] = self.registers[rs1] + self.registers[rs2]
 
@@ -56,6 +63,14 @@ class CPU:
 
     def _xor(self):
         logger.info("Run xor")
+
+    def _li(self, rd: str, imm: str):
+        logger.info(f"Run li with rd={rd}, imm={imm}")
+        rd = self.get_register_index(rd)
+        imm = self.get_imm(imm)
+        self.registers[rd] = Word(imm)
+
+        logger.info(f"Set register x{rd} to: x{rd} = {imm}")
 
 
 if __name__ == "__main__":
