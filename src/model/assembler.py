6fec81236666f7ac89c89e3f09a9481bc9427d61
michisthr <michlstoehr19@icloud.com>
2025-06-01 14:28:31 +0200

    update assembler class

--- src/model/assembler.py
+++ src/model/assembler.py
@@ -1,8 +1,20 @@
+import pprint
 from typing import Dict, List, Tuple
 import re
 
-from helper import LABEL_REGEX
-from helper import INSTRUCTION_REGEX
+LABEL_REGEX: str = re.compile(
+    r"^[a-zA-Z_][a-zA-Z0-9_]*:$"  # (sum:, end:, ...)
+)
+
+INSTRUCTION_REGEX: str = re.compile(
+    r"^\s*"                      # Begin, optional space
+    r"([a-z]+)"                  # Opcode (addi, beq, ...)
+    r"\s+"                       # Space
+    r"([x]\d+|\w+)"              # 1. operand
+    r"(?:\s*,\s*([x]\d+|\w+))?"  # 2. operand (optional)
+    r"(?:\s*,\s*([x]\d+|\w+))?"  # 3. operand (optional)
+    r"\s*(?:#.*)?$"              # comment (otional)
+)
 
 
 class Assembler:
@@ -11,28 +23,47 @@ class Assembler:
         self.symbol_table: Dict[str, int] = {}
 
         # {instruction string, adress}
-        self.instructions: Dict[List[str], int] = {}
+        # Only one adress for each instruction
+        self.instructions: Dict[int, List[str]] = {}
+
+    def parse_symbol_table(self, programm: List[str]) -> Dict[str, int]:
+        adr = 0
+        for _, line in enumerate(programm):
+            line = line.strip()
+            if re.match(LABEL_REGEX, line):
+                line = line.removesuffix(":")
+                if line in self.symbol_table:
+                    raise ValueError(
+                        f"The labels {line} already exists in the symbol_table '{line}:{self.symbol_table.get(line)}'"
+                    )
+                self.symbol_table[line] = adr
+                continue
+            adr += 1
+        return self.symbol_table
+
+    def parse_instructions(self, programm: List[str]) -> Dict[List[str], int]:
+        adr = 0
+        for idx, line in enumerate(programm):
+            line = line.strip()
+            if re.match(LABEL_REGEX, line):
+                continue
+
+            match = re.match(INSTRUCTION_REGEX, line)
+            if not match:
+                raise ValueError(
+                    f"Parsing of programm failed in line {idx}: {line}"
+                    f"It should match {LABEL_REGEX} or {INSTRUCTION_REGEX}"
+                )
+
+            instruction: List[str] = [
+                self.symbol_table.get(group, group) for group in match.groups()
+            ]
+            self.instructions[adr] = instruction
+            adr += 1
+        return self.instructions
 
     def parse_programm(
         self, programm: List[str]
     ) -> Tuple[Dict[str, int], Dict[List[str], int]]:
-        for idx, line in enumerate(programm):
-            if re.match(LABEL_REGEX, line):
-                if line.strip() in self.labels:
-                    raise ValueError(f"The labels already exists: {programm}")
-                self.labels |= {line.strip(), idx}
-                continue
-
-            line = line.split("x")[0].strip()  # remove comment
-            match = re.match(INSTRUCTION_REGEX, line)
-            if match:
-                instructions: List[str] = [group for group in match.groups() if group]
-                self.instructions |= {instructions, idx}
-                continue
-
-            raise ValueError(
-                f"Parsing of programm failed in line {idx}: {line}"
-                f"It should match {LABEL_REGEX} or {INSTRUCTION_REGEX}"
-            )
-
-        return self.symbol_table, self.instructions
+        """Returns symbol_table: Dict[str, int] and instructions: Dict[int, List[str]]"""
+        return self.parse_symbol_table(programm), self.parse_instructions(programm)
